55872369b7d13a8802ce9cc8a35aa305ebec5dcc
anonymousnoot <not.noppawat@gmail.com>
2026-09-18 21:59:28 +0700

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,5 +1,12 @@
-from django.urls import include, path
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path("", include("dashboard.urls")),
+    path('admin/', admin.site.urls),
+    path('', include('dashboard.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.BASE_DIR / 'dashboard' / 'static')
